1046e932534c238d38e69e03088c6ce7bc5424fc
Bogdan <b.sinchuk@itcg.ua>
2019-03-20 20:08:41 +0200

    MongoDB through the JavaScript shell

--- mongodb-in-action.js
+++ mongodb-in-action.js
@@ -37,3 +37,33 @@ db.users.update({ username: "jones" },
   }
 )
 db.users.find({ "favorites.movies": "Casablanca" })
+
+// more advanced updates
+db.users.update({ "favorites.movies": "Casablanca" },
+  { $addToSet: { "favorites.movies": "The Maltese Falcon" } },
+  false,
+  true)
+
+// deleting data
+db.foo.remove()
+db.users.remove({ "favorites.cities": "Cheyenne" })
+db.users.drop()
+
+// indexes
+for (i = 0; i < 20000; i++) {
+  db.numbers.insert({ num: i });
+}
+
+// range queries
+db.numbers.find({ num: { $gt: 19995 } })
+db.numbers.find({ num: { $lt: 5 } })
+
+// Indexing and explain( )
+db.numbers.find({ num: { $gt: 19995 } }).explain("executionStats")
+db.numbers.createIndex({ num: 1 })
+db.numbers.getIndexes()
+
+// getting database information
+show dbs
+show collections
+db.stats()
