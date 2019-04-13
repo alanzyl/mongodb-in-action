2bae272bb8720cb8149571558dd3b13134d34dc0
Bohdan Sinchuk <sinchuk140995@gmail.com>
2019-04-13 19:04:04 +0300

    7-updates-atomic-operations-deletes.js: add Products and categories updates

--- 7-updates-atomic-operations-deletes.js
+++ 7-updates-atomic-operations-deletes.js
@@ -37,9 +37,84 @@ db.users.update({ "_id": user_id },
 
 // Both methods compared
 
-product_id = ObjectId("4c4b1476238d3b4dd5003982")
+product_id = ObjectId("4c4b1476238d3b4dd5003981")
 doc = db.products.findOne({ "_id": product_id })
 doc["total_reviews"] += 1
 doc.products.update({ "_id": product_id }, doc)
 
 db.products.update({ "_id": product_id }, { $inc: { "total_reviews": 1 } })
+
+
+// AVERAGE PRODUCT RATINGS
+
+product_id = ObjectId("4c4b1476238d3b4dd5003981")
+count = 0
+total = 0
+db.reviews.find({ "product_id": product_id }, { "rating": 1 }).forEach(
+  function(review) {
+    total += review.rating;
+    count += 1;
+  }
+)
+average = total/count
+
+// alternative with the aggregation
+stat = db.reviews.aggregate([
+  { $match: { "product_id": product_id } },
+  { $group: { "_id": "product_id",
+              "average": { $avg: "$rating" },
+              "count": { $sum: 1 }, }
+  },
+]).next()
+average = stat.average
+count = stat.count
+
+db.products.update({ "_id": product_id }, { $set: { "total_reviews": count, "average_review": average } })
+
+
+// THE CATEGORY HIERARCHY
+
+var generate_ancestors = function(_id, parent_id) {
+  ancestor_list = []
+  var cursor = db.categories.find({ "_id": parent_id })
+  while (cursor.size() > 0) {
+    parent = cursor.next()
+    ancestor_list.push({ "_id": parent._id, "name": parent.name, "slug": parent.slug })
+    parent_id = parent.parent_id
+    cursor = db.categories.find({ "_id": parent_id })
+  }
+
+  db.categories.update({ "_id": _id }, { $set: { "ancestors": ancestor_list } })
+}
+
+parent_id = ObjectId("6a5b1476238d3b4dd5000048")
+slug = "gardening-child"
+category = {
+  parent_id: parent_id,
+  slug: slug,
+  name: "Gardening child",
+  description: "All gardening implements, tools, seeds, and soil."
+}
+
+db.categories.save(category)  // save puts "_id" in an original document
+
+category = db.categories.findOne({ "slug": slug })
+generate_ancestors(category._id, parent_id)
+
+// change the categories hierarchy
+db.categories.update( { "_id": outdoors_id }, { $set: { "parent_id": gardening_id } } )
+db.categories.find( { "ancestors.id": outdoors_id } ).forEach(
+  function(category) {
+    generate_ancestors(category._id, outdoors_id)
+  }
+)
+
+// change category name
+doc = db.categories.findOne( { "_id": outdoors_id })
+doc.name = "The Great Outdoors"
+db.categories.update( { "_id": outdoors_id }, doc )
+db.categories.update( { "ancestors.id": outdoors_id}, { $set: { "ancestors.$": doc } }, { "multi": true } )
+
+// the positional operator
+db.users.update( { "_id": ObjectId("4c4b1476238d3b4dd5000001"), "addresses.name": "work" },
+                 { $set: { "addresses.$.street": "155 E 31st St." } } )
