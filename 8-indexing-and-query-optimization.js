a7338f095ef99b59b9b2bf0b4d9ab30f9887f78c
Bogdan <b.sinchuk@itcg.ua>
2019-04-24 18:01:19 +0300

    add 8.2 Indexing in practice

--- 8-indexing-and-query-optimization.js
+++ 8-indexing-and-query-optimization.js
@@ -1,3 +1,29 @@
 use e-commerce
 
+// UNIQUE INDEXES
 db.users.createIndex({"username": 1}, {"unique": true})
+
+// SPARSE INDEXES
+db.products.createIndex({"sku": 1}, {"unique": true, "sparse": true})
+db.reviews.createIndex({"user_id": 1}, {"unique": true, "sparse": true})
+
+// HASHED INDEXES
+db.recipes.createIndex({"recipe_name": "hashed"})
+
+
+// 8.2.2 Index administration
+use green
+spec = {
+    "ns": "green.users",
+    "key": {"addresses.zip": 1},
+    "name": "zip"
+}
+db.systems.indexes.insert(spec, true)
+
+db.green.getIndexes()
+
+// BACKGROUND INDEXING
+db.values.createIndex({"open": 1, "close": 1}, {"background": true})
+
+// DEFRAGMENTING
+db.values.reIndex()
